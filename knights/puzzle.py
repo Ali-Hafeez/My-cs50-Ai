998cd29c187558925f2e3d5997f4edbec452d3c3
Ali-Hafeez <ali.hafeez-buttar@city.ac.uk>
2024-07-18 17:12:00 +0100

    Update puzzle.py

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -9,6 +9,14 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+knowledgeBase = And(
+    Or(AKnave, AKnight),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnave, AKnight)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+)
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
@@ -16,7 +24,10 @@ knowledge0 = And(
     # this allows me to be able to filter some true cases to the ones I need
     # then I just use an and gate to get the true value which gives "A is a Knave"
 
-    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), AKnave
+    # knowledgeBase,And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), AKnave
+    knowledgeBase,
+    Implication(AKnight, And(AKnave, AKnight)),
+    Implication(AKnave, Not(And(AKnave, AKnight)))
 
 )
 
@@ -26,14 +37,22 @@ knowledge0 = And(
 knowledge1 = And(
     # Having deduced that A is a knave and B is a knight
     # I need to correctly use the logical gates and get this information through
-    Or(AKnave, BKnave), And(Not(BKnave), BKnight)
+    knowledgeBase,
+    # Or(AKnave, BKnave), And(Not(BKnave), BKnight)
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+    Implication(AKnight, And(AKnave, BKnave))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    And(Or(AKnave, BKnave), And(AKnave, BKnight))
+    knowledgeBase,
+    # And(Or(AKnave, BKnave), And(AKnave, BKnight))
+    Implication(AKnave, Or(And(AKnave, BKnight), And(AKnight, BKnave))),
+    Implication(AKnight, And(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, And(And(AKnight, BKnight), And(AKnave, BKnave)))
 )
 
 # Puzzle 3
@@ -42,8 +61,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # And(AKnight, AKnave), And(And(Not(BKnave), BKnight), And(Not(CKnight), CKnave))
-    And(Or(AKnave, AKnight), AKnave), CKnave, BKnight
+    knowledgeBase,
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(AKnight, Or(AKnave, AKnight)),
+    Implication(AKnave, Not(Or(AKnave, AKnight))),
+    # B says "A said 'I am a knave'."
+    Or(Implication(BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))),
+       Implication(BKnave, Not(Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))))),
+    # B says "C is a knave."
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+    # C says "A is a knight."
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
 )
 
 
